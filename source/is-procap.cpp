3b0b26f59418a521e646e58c420d5bb751a53858
Sebastian Schuberth <sschuberth@gmail.com>
2012-04-12 17:25:13 +0200

    Work-around Pascal Script passing empty PAnsiChar strings as NULL pointers

--- source/is-procap.cpp
+++ source/is-procap.cpp
@@ -9,10 +9,22 @@ extern "C" {
  * the EXPORTS section of the .def file.
  */
 
-bool __stdcall CaptureProcess(char const* exe,char const* arg)
+bool __stdcall CaptureProcess(char const* executable,char const* arguments)
 {
     if (s_procap) {
         return false;
+    }
+
+    // For some reason, Pascal Script passes an empty PAnsiChar string ('') as
+    // a NULL pointer, so work around that case.
+    std::string exe;
+    if (executable) {
+        exe=executable;
+    }
+
+    std::string arg;
+    if (arguments) {
+        arg=arguments;
     }
 
     s_procap=new ProcessCapture();
